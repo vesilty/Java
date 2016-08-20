cd13c5dd5908d22fcdad5e22a8b9936cf5f0dd60
vesilty <veruska@outlook.com>
2016-08-19 21:16:56 -0300

    Commit Inicial

---
+++ src/primeiro/Primeiro.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primeiro;
+
+/**
+ *
+ * @author Veruska
+ */
+public class Primeiro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       //System.out.print("Meu segundo programa em Java!"); //sem quebra de linha    
+      //System.out.print("Meu segundo programa em Java! \n"); //cm \n par aquebrar a linha
+       //System.out.println("Meu primeiro programa em Java!");  //quebra  a linha automatico
+     //  System.out.printf("programação progressiva");
+     //  System.out.printf("Programação progressiva \n", args);
+    // System.out.printf("Programação progressiva"
+      //       +"");
+      
+      //ex2
+        // System.out.print("Veruska silty capellotto"
+        // + "\n CEP:04633-000"
+        // +"\n TEL: 976590254 \n");
+        
+        
+      //ex4
+      /*
+      System.out.println("  Boa noite meu amor!! \n\n"
+              +"Quero que saiba que os melhores dias de minha vida \n"
+              +"tem sido ao seu lado! \n"
+              + "Voce me faz muito feliz!!!\n"
+              + "Te amo!!!!");
+      
+      */
+      
+      //ex6
+        /*
+      System.out.println("xxxxx \n"
+              + "x   x\n"
+              + "x   x\n"
+              + "x   x\n"
+              + "xxxxx"  );
+        */
+        
+        
+        //ex7
+        
+        System.out.println("ALUNO(A)       NOTA"
+                + "\n=========      ====="
+                + "\nALINE          9.0"
+                + "\nMÁRIO          DEZ"
+                + "\nSÉRGIO         4.5"
+                + "\nSHIRLEI        7.0");
+        
+    }
+    
+}
